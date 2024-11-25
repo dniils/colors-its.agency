5d3c666437874a88fe2257dedc688bfb42d9c1d8
dniils <tochka098@gmail.com>
2024-11-25 13:51:23 +0300

    refactor: use direct prop access

--- src/components/ProductFilters.vue
+++ src/components/ProductFilters.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import AppToggle from "./AppToggle.vue";
 
-const props = defineProps<{
+defineProps<{
   filters: Record<string, { name: string; isActive: boolean }>;
 }>();
 </script>
 
 <template>
   <ul class="filters">
-    <li v-for="(filter, key) in props.filters" :key="key">
+    <li v-for="(filter, key) in filters" :key="key">
       <AppToggle :filter="filter" />
     </li>
   </ul>
